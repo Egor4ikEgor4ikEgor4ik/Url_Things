e86f860d99f526d6586b325d44cb488bf6d545a3
Egor4ikEgor4ikEgorik <prodolzitprodolzit@gmail.com>
2023-06-28 18:40:08 +0300

    Added one functional shortening links

--- main.py
+++ main.py
@@ -16,12 +16,25 @@ access_token_bitly=os.getenv("ACCESS_TOKEN_BITLY")
 #response.raise_for_status() 
 #print (response.json()["emails"][0]["email"])
 
+
 print ("1) Сократить ссылку 2) Узнать количество кликов по сокращенной ссылке")
 user_choice=int(input("Выберите функцию:"))
 if user_choice==1:
-    print ("сокращение ссылки")
+    choice=input("Напишите свою ссылку: ")
+    headers = {
+    'Authorization': f'Bearer {access_token_bitly}',
+    'Content-Type': 'application/json',
+    }
+    params = { "long_url": choice}
+    
+    
+    response = requests.post('https://api-ssl.bitly.com/v4/bitlinks', headers=headers, json=params)
+    response.raise_for_status()
+    user_link=response.json()["link"]
+    print (user_link,"Ваша ссылочка")
 else:
     print ("узнать количество кликов")
+
 
 
 
